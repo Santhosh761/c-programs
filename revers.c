33032b6cd9ec19e9aebe94ccccbbe80277152b85
Santhosh761 <santhoshkonatham12@gmail.com>
2024-12-13 11:56:37 +0530

    Create revers.c

---
+++ revers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,rem;
+    int rev=0;
+    printf("Enter n value");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("The rev is %d",rev);
+}
